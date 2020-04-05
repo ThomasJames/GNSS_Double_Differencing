356cc0445789a0ac61ffe6a8199fa85b8edcbbbe
Thomas James <42183357+ThomasJames@users.noreply.github.com>
2020-04-05 22:41:12 +0100

    Minor Adjustment

--- Computations.py
+++ Computations.py
@@ -232,7 +232,7 @@ if __name__ == "__main__":
                 yticklabels=False,
                 cmap='Pastel1')
 
-    plt.title('ATWA^-1 Matrix')
+    plt.title('(ATWA)^-1 Matrix')
     plt.savefig("Matrix_Output/Inverse_ATWA.png")
     plt.show()
 
